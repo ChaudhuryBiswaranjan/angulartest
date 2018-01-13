de62768a852555e3d0a9e76f37368e8b5fa02abd
biswaranjan <biswaranjanchaudhury@gmail.com>
2018-01-13 21:40:07 +0530

    module 1 solution

---
+++ module1-solution/scripts/app.js
@@ -0,0 +1,39 @@
+(function () {
+    'use strict';
+    
+    angular.module('LunchCheck', [])
+    
+    .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope){
+        var enjoyMessage="Enjoy!";
+        var tooMuchMessage="Too much!";
+        var invalidInputMessage="Please enter data first";
+
+        $scope.inputTextBordercolor="green";
+        
+        
+        $scope.message = "";
+        $scope.itemListInput="";
+        $scope.checkIfTooMuch = function () {
+            $scope.inputTextBordercolor="green";
+            var itemArray = $scope.itemListInput.split(',');
+            itemArray=itemArray.filter(item => item.length > 0);
+            if(itemArray.length === 0 || $scope.itemListInput === ""){
+                $scope.message = invalidInputMessage;
+                $scope.inputTextBordercolor="red";
+            }else if(itemArray.length <= 3){
+                $scope.message = enjoyMessage;
+            }else {
+                $scope.message = tooMuchMessage;
+            }
+        };
+        $scope.resetMessage = function() {
+            $scope.inputTextBordercolor="green";
+            $scope.message="";
+        };
+
+    }
+    
+    })();
+    
